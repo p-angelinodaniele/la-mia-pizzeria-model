1365c5e6e3331ab98a1d9b0b83f285705fe7e5e6
P.angelino <p.angelinodaniele@gmail.com>
2022-05-10 14:20:03 +0200

    ancora da finire

---
+++ la-mia-pizzeria-static/Controllers/PizzeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.Utils;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> pizze = PizzaData.GetPizze();
+            return View("HomePage", pizze);
+        }
+
+        
+
+
+
+
+    }
+}
